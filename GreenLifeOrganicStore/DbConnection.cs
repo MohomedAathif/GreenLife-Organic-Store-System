8d8ee5a4a1c0eecf399324afb0866fae92490b9f
Mohomed Aathif <maathif28@gmail.com>
2026-08-12 20:42:11 +0530

    feat: add MySQL database schema and database connection

---
+++ GreenLifeOrganicStore/DbConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace GreenLifeOrganicStore
+{
+    public static class DbConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(
+                "Data Source=DESKTOP-RTT32SQ\\SQLEXPRESS;Initial Catalog=GreenLifeDB;Integrated Security=True;Encrypt=False;"
+            );
+        }
+    }
+}
