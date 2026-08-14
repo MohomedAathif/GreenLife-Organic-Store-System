cf7540eccb125fde65a8075c451a1ea7e6f837af
Mohomed Aathif <maathif28@gmail.com>
2026-08-14 20:15:28 +0530

    feat: implement shopping cart management

---
+++ GreenLifeOrganicStore/CartItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenLifeOrganicStore
+{
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        
+        public string ProductName
+        {
+            get { return Product.Name; }
+        }
+        public int Quantity { get; set; }
+        public decimal SubTotal
+        {
+            get { return Product.FinalPrice * Quantity; }
+        }
+    }
+}
